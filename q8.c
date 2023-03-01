afd782ee5d5f995b66c654d2da531aecaff5f82e
Samuel-ifce <126632335+Samuel-ifce@users.noreply.github.com>
2023-02-28 23:03:17 -0300

    Add files via upload

---
+++ q8.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int num;
+    puts("Digite um número:");
+    scanf("%d", &num);
+    printf("O quadrado de %d é %d\n", num, num*num);
+    
+
+    return 0;
+}
