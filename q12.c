79b84b33bbecaaf3f38b329e67fb851a22f7301a
Samuel-ifce <126632335+Samuel-ifce@users.noreply.github.com>
2023-02-28 23:05:11 -0300

    Add files via upload

---
+++ q12.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    int num1, num2;
+    puts("Digite dois números: ");
+    scanf("%d %d", &num1, &num2);
+    printf("Soma- %d\nProduto-%d\nDiferença- %d\nQuociente- %d\nResto- %d\n",num1+num2, num1*num2, num1-num2, num1/num2, num1%num2);
+
+
+
+
+
+    return 0;
+}
