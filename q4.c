45d2ff68c799a449c4593eecfb389d07b4321ecd
Samuel-ifce <126632335+Samuel-ifce@users.noreply.github.com>
2023-02-28 22:58:33 -0300

    Add files via upload

---
+++ q4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int a;
+    float b;
+    scanf("%d",&a);
+    b = a;
+    printf("O triplo de %d é %d\n seu quadrado é %d\n Seu meio é %.1f\n",a, a*3, a*a, b/2 );
+
+
+
+
+
+    return 0;
+}
