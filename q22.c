4956a43ff47fe5ac6cbb3bbbdfce91b122ead681
Samuel-ifce <126632335+Samuel-ifce@users.noreply.github.com>
2023-04-05 20:07:24 -0300

    Add files via upload

---
+++ q22.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+    printf("Valor de 'A': %d\n", 'A'); 
+    printf("Valor de 'B': %d\n", 'B'); 
+    printf("Valor de 'C': %d\n", 'C'); 
+    printf("Valor de 'a': %d\n", 'a'); 
+    printf("Valor de 'b': %d\n", 'b'); 
+    printf("Valor de 'c': %d\n", 'c'); 
+    printf("Valor de '0': %d\n", '0'); 
+    printf("Valor de '1': %d\n", '1'); 
+    printf("Valor de '2': %d\n", '2'); 
+    printf("Valor de '$': %d\n", '$');
+    printf("Valor de '*': %d\n", '*'); 
+    printf("Valor de '+': %d\n", '+'); 
+    printf("Valor de '/': %d\n", '/'); 
+    printf("Valor de espaço em branco: %d\n", ' ');
+
+    
+
+
+
+    return 0;
+}
